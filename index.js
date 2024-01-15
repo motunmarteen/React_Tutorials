d0394d0cfbe3618cae0f2f3d48ef68509cb529fb
Marteen Motun Mubaraq <motunmarteen@gmail.com>
2024-01-15 09:54:46 +0100

    This is the updated code

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Challenge: 
+
+// Create a navbar in JSX:
+//     - Use the semantic `nav` element as the parent wrapper
+//     - Have an h1 element with the brand name of your "website"
+//     - Insert an unordered list for the other nav elements
+//         - Inside the `ul`, have three `li`s for "Pricing",
+//         "About", and "Contact"
+//     - Don't worry about styling yet - it'll just be plain-looking HTML for now
+
+
 const navbar = (
     <nav className="navbar">
         <h1>getLume</h1>
